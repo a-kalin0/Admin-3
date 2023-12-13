d3e6aad6dfebe809799f0448d80b083312ee71a0
Arthur Kalinowska <114411228+a-kalin0@users.noreply.github.com>
2023-12-13 18:32:29 +0100

    Ajout de deleteUser.ps1

---
+++ Admin/Scripts/DeleteUser.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module ActiveDirectory
+
+function Remove-UserFromCSV {
+    param(
+        [string]$csvPath
+    )
+
+    $usersData = Import-Csv $csvPath
+
+    foreach ($userData in $usersData) {
+        $username = $userData.Username
+
+        try {
+            # Suppression du compte utilisateur
+            Remove-ADUser -Identity $username -Confirm:$false
+            Write-Host "Le compte de $username a été supprimé avec succès."
+        } catch {
+            Write-Host "Erreur lors de la suppression du compte de $username : $_"
+        }
+    }
+}
+
+Remove-UserFromCSV -csvPath "C:\Users\Administrator\Desktop\Admin\Scripts\Users.csv"
