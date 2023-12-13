0458da7f5d39dff55d298612c280ca4375aee64e
Arthur Kalinowska <114411228+a-kalin0@users.noreply.github.com>
2023-12-13 14:48:44 +0100

    Ajout des nouveaux fichiers de scripting corrigés

---
+++ Admin/Scripts/CreateUser.ps1
@@ -0,0 +1,35 @@
+﻿# Création automatiquement des comptes utilisateurs Active Directory.
+
+Import-Module ActiveDirectory
+
+function Create-UserFromCSV {
+    param(
+        [string]$csvPath
+    )
+
+    $usersData = Import-Csv $csvPath
+
+    foreach ($userData in $usersData) {
+        $username = $userData.Username
+        $password = $userData.Password
+        $email = $userData.Email
+
+        # Vérifier si l'utilisateur existe déjà
+        $userExists = Get-ADUser -Filter "SamAccountName -eq '$username'" -ErrorAction SilentlyContinue
+
+        if ($userExists) {
+            Write-Host "Le user $username a déjà été créé comme il faut"
+        } else {
+            try {
+                New-ADUser -SamAccountName $username -UserPrincipalName "$username@your_domain.com" -Name $username -GivenName $username -Surname "User" -EmailAddress $email -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true
+                Write-Host "Bravo ! Le user $username a été créé !"
+            } catch [Microsoft.ActiveDirectory.Management.ADPasswordComplexityException] {
+                Write-Host "Le mot de passe ne respecte pas les conditions pour $username"
+            } catch {
+                Write-Host "Une erreur est survenue lors de la création du user $username : $_"
+            }
+        }
+    }
+}
+
+Create-UserFromCSV -csvPath "C:\Users\Administrator\Desktop\Admin\Scripts\Users.csv"
---
+++ Admin/Scripts/ModifyUser.ps1
@@ -0,0 +1,39 @@
+﻿Import-Module ActiveDirectory
+
+function Modify-UserFromCSV {
+    param(
+        [string]$csvPath
+    )
+
+    $usersData = Import-Csv $csvPath
+
+    foreach ($userData in $usersData) {
+        $username = $userData.Username
+        $attribute = $userData.Attribute
+        $newValue = $userData.NewValue
+
+        switch ($attribute) {
+            "Password" {
+                $securePassword = ConvertTo-SecureString $newValue -AsPlainText -Force
+                Set-ADAccountPassword -Identity $username -NewPassword $securePassword -Reset
+                Write-Host "Le mot de passe pour le compte $username a été modifié !"
+            }
+            "Email" {
+                Set-ADUser -Identity $username -EmailAddress $newValue
+                Write-Host "L'adresse e-mail pour le compte $username a été modifiée !"
+            }
+            "Username" {
+                $newSamAccountName = $newValue
+                $newUserPrincipalName = "$newValue@3TL2-7V2.lab"
+                Get-ADUser -Identity $username | Set-ADUser -SamAccountName $newSamAccountName -UserPrincipalName $newUserPrincipalName
+                Write-Host "Le nom d'utilisateur pour $username a été changé en $newSamAccountName !"
+            }
+
+            default {
+                Write-Host "Attribut non reconnu : $attribute"
+            }
+        }
+    }
+}
+
+Modify-UserFromCSV -csvPath "C:\Users\Administrator\Desktop\Admin\Scripts\NewUsers.csv"
